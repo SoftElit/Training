300f27125a4ca0295d2696fe08a705f47df60edd
SoftElit <mobcomm@yandex.ru>
2021-01-06 21:59:34 +0300

    Item 2 of the task 2.2. function mySortForKey($a, $b)

--- 2.php
+++ 2.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Создать файл 2.php и в нем реализовать:
-- функцию convertString($a, $b). Результат ее выполнение: если в строке $a содержится 2 и более подстроки $b, то во втором месте заменить подстроку $b на инвертированную подстроку.
+
+
+функцию convertString($a, $b). Результат ее выполнение: если в строке $a содержится 2 и более подстроки $b, то во втором месте заменить подстроку $b на инвертированную подстроку.
 */
-
-
 function mb_strrev($string)
 {
     $string = strrev(mb_convert_encoding($string, 'UTF-16LE', 'UTF-8'));
@@ -19,7 +19,6 @@ function mb_strrev($string)
 function convertString($a, $b)
 {
 	if(mb_substr_count($a, $b) >= 2) {
-		
 		$a = 
 			strstr($a, $b, true) . 
 			$b . 
@@ -27,26 +26,6 @@ function convertString($a, $b)
 			mb_strrev($b) . 
 			mb_substr(strstr(mb_substr(strstr($a, $b), mb_strlen($b)), $b), mb_strlen($b));
 		
-		/*
-		$a0 = strstr($a, $b, true); //'начало строки, '
-		
-		$strstr1 = strstr($a, $b); //'иголка в стоге, середина строки, иголка в стоге, конец строки'
-		
-		//$a1 = strstr(mb_substr($strstr1, mb_strlen($b)), $b, true); //', середина строки, '
-		$a1 = strstr(mb_substr(strstr($a, $b), mb_strlen($b)), $b, true); //', середина строки, '
-		
-		$a2 = mb_strrev($b); //инвертирование строки 'иголка в стоге' (обратная последовательность символов)
-		
-		//$a3 = strstr(mb_substr($strstr1, mb_strlen($b)), $b); //'иголка в стоге, конец строки'
-		$a3 = strstr(mb_substr(strstr($a, $b), mb_strlen($b)), $b); //'иголка в стоге, конец строки'
-		
-		//$a4 = mb_substr($a3, mb_strlen($b)); //', конец строки'
-		$a4 = mb_substr(strstr(mb_substr(strstr($a, $b), mb_strlen($b)), $b), mb_strlen($b)); //', конец строки'
-		
-		//strstr(substr(strstr($a, $b), strlen($b)), );
-		
-		$a = $a0 . $b . $a1 . $a2 . $a4; //собираем/сохраняем полученные подстроки обратно в строку
-		*/
 		return $a;
 	}
 	
@@ -59,3 +38,31 @@ $b = 'иголка в стоге';
 //$a = '1, 123, середина строки, 123, 123, конец строки.';
 //$b = '123';
 echo convertString($a, $b);
+echo '<br><br>';
+
+
+/* функию mySortForKey($a, $b). $a – двумерный массив вида [['a'=>2,'b'=>1],['a'=>1,'b'=>3]], $b – ключ вложенного массива. 
+Результат ее выполнения: двумерном массива $a отсортированный по возрастанию значений для ключа $b. 
+В случае отсутствия ключа $b в одном из вложенных массивов, выбросить ошибку класса Exception с индексом неправильного массива. */
+function mySortForKey($a, $b)
+{
+	foreach($a as $key => $inputArray) {
+		if(!array_key_exists($b, $inputArray)) {
+			throw new Exception(" Отсутствует ключ \"$b\" в массиве \"$key\"");
+		}
+	};
+	
+		usort($a, function($x1, $x2) use ($b) {
+			return $x1[$b] <=> $x2[$b];
+		});
+		
+		return $a;
+};
+
+$a = [['a' => 2,'b' => 1],['a' => 1,'b' => 3]];
+
+try {
+print_r(mySortForKey($a, 'a'));
+} catch (Exception $e) {
+    echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
+};
